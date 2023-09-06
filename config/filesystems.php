d5aa810335e6cdb629d4120ed0c41320b5a65a98
colondum <decarabian21@gmail.com>
2023-09-06 11:40:40 +0700

    update

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,7 +58,7 @@ return [
 
         'custom' => [
             'driver' => 'local',
-            'root' => base_path('../manifestasi.my.id'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
